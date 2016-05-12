7fdd7c3e1372aa5f725500d48ee9c51109204075
do9iigane <dokumegane@interestic.com>
2016-05-12 15:26:53 +0900

    #3 更新対象のカラム追加、該当カラムのユニーク化 ## add - url - team - location ## mod - name - email

---
+++ database/migrations/2016_05_12_121916_modify_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('url')->after('email');
+            $table->string('team')->after('url');
+            $table->string('location')->after('team');
+            $table->unique(['name','email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['url','team', 'location']);
+        });
+    }
+}
